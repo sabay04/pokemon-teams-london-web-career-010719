62ebf4755a8727d0f2e6059796f559c940f1a185
github username <github email address>
2019-02-21 14:57:25 +0000

    added trainer cards

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,42 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+function getTrainer(){
+
+    return fetch(TRAINERS_URL).then(resp => resp.json())
+
+}
+
+
+
+getTrainer().then(createCardBody)
+
+
+const mainEl = document.querySelector('main')
+
+
+function createCardBody(trainers) {
+
+    trainers.forEach( trainer => {
+
+    const cardDivEl = document.createElement('div')
+    cardDivEl.className = 'card'
+    const cardP = document.createElement('p')
+    const addBtn = document.createElement('button')
+    const cardUl = document.createElement('ul')
+    const pokemonli = document.createElement('li')
+
+    cardP.innerText = trainer.name
+    addBtn.innerText = 'Add Pokemon'
+
+    mainEl.appendChild(cardDivEl)
+    cardDivEl.appendChild(cardP)
+    cardDivEl.appendChild(addBtn)
+    cardDivEl.appendChild(cardUl)
+
+
+  })
+
+}
